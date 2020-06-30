ab26174d07a12a1c56aa311ca36331291133357f
Kate F <follett.katie@gmail.com>
2020-06-30 02:41:21 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import manageUsers from './reducers/manageUsers'
 
-
-// add imports and code
-
-
 ReactDOM.render(<Provider = {store}>
-  // add imports and code
   <App />
   // add imports and code
   ,
