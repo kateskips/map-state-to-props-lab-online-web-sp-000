158ef8a453e6de7804bf00b4bff79bd43a2a4075
Kate F <follett.katie@gmail.com>
2020-06-30 02:39:49 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
+import { store } from 'react-redux'
 
 import manageUsers from './reducers/manageUsers'
 
